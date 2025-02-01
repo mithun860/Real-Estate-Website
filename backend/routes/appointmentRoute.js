1543ac9c6f32975039eccf2fd6036a8f7b3a3535
Aayush <vaghelaaayush412@gmail.com>
2025-02-01 18:23:25 +0530

    update

--- backend/routes/appointmentRoute.js
+++ backend/routes/appointmentRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { scheduleViewing } from '../controller/appointmentController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/authmiddleware.js';
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded());
+
+
 
 // CORS Configuration with error handling
 app.use((req, res, next) => {
