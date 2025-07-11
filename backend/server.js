975d9898bebfc408c0d1b1aad21a6dcf71fb5ae5
mithun856 <uppadhyaymithun63@gmail.com>
2025-07-11 16:16:45 +0530

    web connections

--- backend/server.js
+++ backend/server.js
@@ -45,9 +45,8 @@ app.use(cors({
     'http://localhost:4000',
     'http://localhost:5174',
     'http://localhost:5173',
-    'https://buildestate.vercel.app',
-    'https://real-estate-website-admin.onrender.com',
-    'https://real-estate-website-backend-zfu7.onrender.com',
+    'https://splrdevelopers-gules.vercel.app/',
+    'https://real-estate-website-admin.vercel.app/',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'], // Added HEAD
